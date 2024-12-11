92094e524bd9ca8efa71116d81f8ec1b64b5f537
Chancellion <ivanchenko160306@gmail.com>
2024-12-11 08:27:46 +0200

    Add project files.

---
+++ Lab-3/Task-1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EcosystemSimulation
+{
+    // Базовий клас для всіх живих організмів
+    public class LivingOrganism
+    {
+        public int Energy { get; set; }
+        public int Age { get; set; }
+        public int Size { get; set; }
+
+        public LivingOrganism(int energy, int age, int size)
+        {
+            Energy = energy;
+            Age = age;
+            Size = size;
+        }
+
+        public bool IsAlive() => Energy > 0;
+
+        public void GrowOlder()
+        {
+            Age++;
+        }
+
+        public void DecreaseEnergy(int amount)
+        {
+            Energy -= amount;
+        }
+    }
+    public interface IReproducible
+    {
+        LivingOrganism Reproduce();
+    }
+    public interface IPredator
+    {
+        void Hunt(LivingOrganism prey);
+    }
+    public class Animal : LivingOrganism, IPredator
+    {
+        public int Speed { get; set; }
+        public bool IsCarnivorous { get; set; }
+
+        public Animal(int energy, int age, int size, int speed, bool isCarnivorous)
+            : base(energy, age, size)
+        {
+            Speed = speed;
+            IsCarnivorous = isCarnivorous;
+        }
+        public void Hunt(LivingOrganism prey)
+        {
+            if (IsCarnivorous && prey is Animal preyAnimal && preyAnimal.IsAlive())
+            {
+                Energy += 10;
+                preyAnimal.DecreaseEnergy(5);
+                Console.WriteLine($"{this.GetType().Name} hunted {prey.GetType().Name} and gained energy.");
+            }
+            else
+            {
+                Console.WriteLine($"{this.GetType().Name} cannot hunt.");
+            }
+        }
+    }
+    public class Plant : LivingOrganism
+    {
+        public int PhotosynthesisRate { get; set; }
+
+        public Plant(int energy, int age, int size, int photosynthesisRate)
+            : base(energy, age, size)
+        {
+            PhotosynthesisRate = photosynthesisRate;
+        }
+
+        public void Photosynthesize()
+        {
+            Energy += PhotosynthesisRate;
+            Console.WriteLine($"{this.GetType().Name} photosynthesized and gained energy.");
+        }
+    }
+    public class Microorganism : LivingOrganism, IReproducible
+    {
+        public int ReproductionRate { get; set; }
+        public Microorganism(int energy, int age, int size, int reproductionRate)
+            : base(energy, age, size)
+        {
+            ReproductionRate = reproductionRate;
+        }
+        public LivingOrganism Reproduce()
+        {
+            if (Energy >= 5)
+            {
+                Energy -= 2;
+                var newMicroorganism = new Microorganism(3, 0, 1, ReproductionRate);
+                Console.WriteLine($"{this.GetType().Name} reproduced and created a new microorganism.");
+                return newMicroorganism;
+            }
+            else
+            {
+                Console.WriteLine($"{this.GetType().Name} doesn't have enough energy to reproduce.");
+                return null;
+            }
+        }
+    }
+    public class Ecosystem
+    {
+        public List<LivingOrganism> Organisms { get; set; }
+
+        public Ecosystem()
+        {
+            Organisms = new List<LivingOrganism>();
+        }
+
+        public void AddOrganism(LivingOrganism organism)
+        {
+            Organisms.Add(organism);
+        }
+
+        public void Simulate()
+        {
+            List<LivingOrganism> newOrganisms = new List<LivingOrganism>();  // Список для нових організмів
+
+            foreach (var organism in Organisms)
+            {
+                if (organism is Animal animal && animal is IPredator predator)
+                {
+                    var prey = FindPrey(animal);
+                    if (prey != null)
+                    {
+                        predator.Hunt(prey);
+                    }
+                }
+                else if (organism is Plant plant)
+                {
+                    plant.Photosynthesize();
+                }
+                else if (organism is Microorganism microorganism && microorganism is IReproducible reproducible)
+                {
+                    var newOrganism = reproducible.Reproduce();
+                    if (newOrganism != null)
+                    {
+                        newOrganisms.Add(newOrganism);
+                    }
+                }
+            }
+            Organisms.AddRange(newOrganisms);
+        }
+        private LivingOrganism FindPrey(Animal predator)
+        {
+            foreach (var organism in Organisms)
+            {
+                if (organism is Animal prey && prey != predator && prey.IsAlive())
+                {
+                    return prey;
+                }
+            }
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Данiїл Iванченко, КIб-1-23-4.0д");
+            var lion = new Animal(energy: 50, age: 5, size: 100, speed: 60, isCarnivorous: true);
+            var deer = new Animal(energy: 30, age: 3, size: 50, speed: 40, isCarnivorous: false);
+            var tree = new Plant(energy: 20, age: 10, size: 5, photosynthesisRate: 5);
+            var bacteria = new Microorganism(energy: 10, age: 1, size: 1, reproductionRate: 2);
+
+            var ecosystem = new Ecosystem();
+            ecosystem.AddOrganism(lion);
+            ecosystem.AddOrganism(deer);
+            ecosystem.AddOrganism(tree);
+            ecosystem.AddOrganism(bacteria);
+            ecosystem.Simulate();
+        }
+    }
+}
---
+++ Task-2/Task-2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Компютер
+{
+    public string IP { get; set; }
+public string Потужність { get; set; }
+public string ТипОС { get; set; }
+public Компютер(string ip, string потужність, string типОС)
+    {
+    IP = ip;
+    Потужність = потужність;
+    ТипОС = типОС;
+}
+public override string ToString()
+{
+    return $"IP: {IP}, Потужність: {Потужність}, Тип ОС: {ТипОС}";
+}
+}
+public class Сервер : Компютер
+{
+    public Сервер(string ip, string потужність, string типОС) : base(ip, потужність, типОС) { }
+}
+public class РобочаСтанція : Компютер
+{
+    public РобочаСтанція(string ip, string потужність, string типОС) : base(ip, потужність, типОС) { }
+}
+public class Маршрутизатор : Компютер
+{
+    public Маршрутизатор(string ip, string потужність, string типОС) : base(ip, потужність, типОС) { }
+}
+public interface IConnectable
+{
+    void Підключитися(Компютер компютер);
+    void Відключитися(Компютер компютер);
+    void ПередатиДані(Компютер компютер, string дані);
+}
+public class Мережа : IConnectable
+{
+    private List<Компютер> компютери = new List<Компютер>();
+    public void ДодатиКомпютер(Компютер компютер)
+    {
+        компютери.Add(компютер);
+        Console.WriteLine($"Додано: {компютер}");
+    }
+    public void Підключитися(Компютер компютер)
+    {
+        Console.WriteLine($"{компютер} підключено до мережі.");
+    }
+    public void Відключитися(Компютер компютер)
+    {
+            Console.WriteLine($"{компютер} відключено від мережі.");
+    }
+    public void ПередатиДані(Компютер компютер, string дані)
+    {
+                Console.WriteLine($"Дані '{дані}' передано до {компютер}.");
+    }
+}
+public class Програма
+    {
+        public static void Main(string[] args)
+        {
+            Мережа мережа = new Мережа();
+        Console.WriteLine("Данiїл Iванченко, КIб-1-23-4.0д");
+        Сервер сервер = new Сервер("192.168.1.1", "Висока", "Linux");
+            РобочаСтанція робочаСтанція = new РобочаСтанція("192.168.1.2", "Середня", "Windows");
+            Маршрутизатор маршрутизатор = new Маршрутизатор("192.168.1.3", "Низька", "Firmware");
+
+            мережа.ДодатиКомпютер(сервер);
+            мережа.ДодатиКомпютер(робочаСтанція);
+            мережа.ДодатиКомпютер(маршрутизатор);
+    
+        мережа.Підключитися(сервер);
+            мережа.ПередатиДані(робочаСтанція, "Привіт, світ!");
+            мережа.Відключитися(маршрутизатор);
+        }
+    }
